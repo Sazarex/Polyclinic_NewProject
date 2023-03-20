cebcc8f670c0a052ec7a28226234e52af506605b
Влас Даниил <shotstrike90@gmail.com>
2023-03-20 17:32:32 +0200

    Добавил первые команды и обработчик для медиатра

---
+++ MediatorInfrastructure/Commands/Accounts/CreateAccountCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace MediatorInfrastructure.Commands.Accounts
+{
+    public class CreateAccountCommand: IRequest<int>
+    {
+        public string Username { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
---
+++ MediatorInfrastructure/Commands/Accounts/CreateAccountCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace MediatorInfrastructure.Commands.Accounts
+{
+    public class CreateAccountCommandHandler : IRequestHandler<CreateAccountCommand, int>
+    {
+        public async Task<int> Handle(CreateAccountCommand request, CancellationToken cancellationToken)
+        {
+
+        }
+    }
+}
